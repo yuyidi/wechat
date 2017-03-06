6649773c40e983f019367a2a2e98c4bd8817fcfa
yuyidi <yuyidi0630@163.com>
2017-03-06 11:40:38 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,7 @@
+> 微信项目
+* wechat-core
+
+> 项目依赖说明
+
+> 项目架构
+![wechat](http://mmbiz.qpic.cn/mmbiz_png/PiajxSqBRaEIQxibpLbyuSK9XkjDgZoL0xnC7SUbrIRwI8NhEGFeax6HoPcTMDqKGYxaSoNqBwocrj70Pt1EcKnQ/0?wx_fmt=png)
---
+++ wechatcore/src/main/java/com/icloudcar/yukuai/core/execption/WechatExecption.java
@@ -0,0 +1,14 @@
+package com.icloudcar.yukuai.core.execption;
+
+/**
+ * 微信运行时异常
+ * Created by yuyidi on 2017/3/3.
+ * package：com.icloudcar.yukuai.core.execption
+ */
+public class WechatExecption extends Exception {
+
+
+    public WechatExecption(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
