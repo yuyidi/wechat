8c262c8a585dbe080be864fde4cc2242b20b3f44
yuyidi <yuyidi0630@163.com>
2017-03-07 10:14:06 +0800

    添加log4j2 配置

---
+++ wechat-core/src/main/java/com/icloudcar/yukuai/core/TokenManager.java
@@ -0,0 +1,16 @@
+package com.icloudcar.yukuai.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 微信Token管理器
+ * Created by yuyidi on 2017/3/6.
+ * package：com.icloudcar.yukuai.core
+ */
+public class TokenManager {
+    public static Logger logger = LoggerFactory.getLogger(TokenManager.class);
+    public static void main(String[] args) {
+        logger.info("开始微信开发");
+    }
+}
